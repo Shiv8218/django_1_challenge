0b2ab10fcb5d20e2f859c126a877f9c6b630b094
Shiv8218 <Shiv8218.pratap@gmail.com>
2023-11-26 13:18:51 +0530

    Added dynamic values

--- challenges/views.py
+++ challenges/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("This works")
+def monthly_challenge(request, month):
+    challenge = "None"
+    if month == "january":
+        challenge = "Eat not meat for the entire month !"
+    elif month == "february":
+        challenge = "Walk for at least 20 minutes everyday !"
+    elif month == "march":
+        challenge = "Learn for at least 20 minutes everyday !"
+    else:
+        return HttpResponseNotFound("This month is not supported !")
+
+    return HttpResponse(challenge)
