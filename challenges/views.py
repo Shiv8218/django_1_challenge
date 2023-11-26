4b5cd1fe8bf57f40547607d6f842d00dff551855
Shiv8218 <Shiv8218.pratap@gmail.com>
2023-11-26 18:42:31 +0530

    add fully dynamic features by using "reverse"

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     "january": "Eat not meat for the entire month !",
@@ -25,7 +26,8 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound("This month is not supported")
 
     redirect_month = months[month-1]
-    return HttpResponseRedirect('/challenges/' + redirect_month)
+    redirect_path = reverse("month-challenge", args=[redirect_month])  # /challenge/january
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
