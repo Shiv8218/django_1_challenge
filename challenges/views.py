a328943ec080e213f6f6c75bbc95d0a44ef2ee16
Shiv8218 <Shiv8218.pratap@gmail.com>
2023-11-27 23:17:07 +0530

    Added many functionality

--- challenges/views.py
+++ challenges/views.py
@@ -14,25 +14,35 @@ monthly_challenges = {
     "september": "Eat not meat for the entire month !",
     "october": "Eat not meat for the entire month !",
     "november": "Eat not meat for the entire month !",
-    "december": "Learn for at least 20 minutes everyday !"
+    "december": None
 }
 
 # Create your views here.
 
 
+def index(request):
+    months = list(monthly_challenges.keys())
+
+    return render(request, "challenges/index.html", {"months":months})
+
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
     if month > len(months):
-        return HttpResponseNotFound("This month is not supported")
+        return HttpResponseNotFound("<h1>This month is not supported</h1>")
 
     redirect_month = months[month-1]
     redirect_path = reverse("month-challenge", args=[redirect_month])  # /challenge/january
+    
     return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        return render(request, "challenges/challenge.html", {
+            "text":challenge_text,
+            "month" : month
+        })
     except:
-        return HttpResponseNotFound("This month is not supported !")
+        return HttpResponseNotFound("<h1>This month is not supported !</h1>")
