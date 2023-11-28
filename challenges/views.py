660c2e4c23e9e15286f3a988f6e0ab813af80d46
Shiv8218 <Shiv8218.pratap@gmail.com>
2023-11-28 22:56:31 +0530

    added final stage of project

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": "Eat not meat for the entire month !",
@@ -45,4 +46,4 @@ def monthly_challenge(request, month):
             "month" : month
         })
     except:
-        return HttpResponseNotFound("<h1>This month is not supported !</h1>")
+        raise Http404()
